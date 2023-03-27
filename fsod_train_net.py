50dedb92a345b1becfda03f977195c9037c46ae0
keith11119 <keithwuck@gmail.com>
2023-03-27 09:58:23 +0100

    Gradient accumulation key register

--- fsod_train_net.py
+++ fsod_train_net.py
@@ -171,6 +171,11 @@ def setup(args):
     cfg = get_cfg()
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
+
+    # Register the GRADIENT_ACCUMULATION_STEPS key
+    cfg.SOLVER.register_key("GRADIENT_ACCUMULATION_STEPS")
+    cfg.SOLVER.GRADIENT_ACCUMULATION_STEPS = 1
+
     cfg.freeze()
     default_setup(cfg, args)
 
