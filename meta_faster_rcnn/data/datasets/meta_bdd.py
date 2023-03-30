4878c27aa186bc73ae2cd54b47a36bae318626d8
keith11119 <keithwuck@gmail.com>
2023-03-30 14:44:33 +0100

    register data

--- meta_faster_rcnn/data/datasets/meta_bdd.py
+++ meta_faster_rcnn/data/datasets/meta_bdd.py
@@ -27,42 +27,114 @@ def load_filtered_bdd_instances(
         dirname: Contain "Annotations", "ImageSets", "JPEGImages"
         split (str): one of "train", "test", "val", "trainval"
     """
-    with PathManager.open(os.path.join(dirname, "ImageSets", "Main",
-                                       split + ".txt")) as f:
-        fileids = np.loadtxt(f, dtype=np.str)
+    is_shots = "shot" in name
+    if is_shots:
+        fileids = {}
+        split_dir = os.path.join("datasets", "bdd", "bddsplit")
+        if "seed" in name:
+            shot = name.split('_')[-2].split('shot')[0]
+            seed = int(name.split('_seed')[-1])
+            split_dir = os.path.join(split_dir, "seed{}".format(seed))
+        else:
+            shot = name.split('_')[-1].split('shot')[0]
+        for cls in classnames:
+            with PathManager.open(os.path.join(split_dir,
+                    "box_{}shot_{}_train.txt".format(shot, cls))) as f:
+                fileids_ = np.loadtxt(f, dtype=np.str).tolist()
+                if isinstance(fileids_, str):
+                    fileids_ = [fileids_]
+                fileids_ = [fid.split('/')[-1].split('.jpg')[0] \
+                                for fid in fileids_]
+                fileids[cls] = fileids_
+                # print("file {} has {} lines".format(os.path.join(split_dir,
+                #     "box_{}shot_{}_train.txt".format(shot, cls)), len(fileids_)))
+    else:
+        with PathManager.open(os.path.join(dirname, "ImageSets", "Main",
+                                           split + ".txt")) as f:
+            fileids = np.loadtxt(f, dtype=np.str)
 
     dicts = []
+    if is_shots:
+        for cls, fileids_ in fileids.items():
+            dicts_ = []
+            tot_instance = 0
+            for fileid in fileids_:
+                dirname = os.path.join("datasets", "bdd", "BDD")
+                anno_file = os.path.join(dirname, "Annotations", fileid + ".xml")
+                jpeg_file = os.path.join(dirname, "JPEGImages", fileid + ".jpg")
 
-    for fileid in fileids:
-        anno_file = os.path.join(dirname, "Annotations", fileid + ".xml")
-        jpeg_file = os.path.join(dirname, "JPEGImages", fileid + ".jpg")
+                tree = ET.parse(anno_file)
+                r = {
+                    "file_name": jpeg_file,
+                    "image_id": fileid,
+                    "height": int(tree.findall("./size/height")[0].text),
+                    "width": int(tree.findall("./size/width")[0].text),
+                }
 
-        tree = ET.parse(anno_file)
+                instances = []
+                for obj in tree.findall("object"):
+                    cls_ = obj.find("name").text
+                    # difficult = int(obj.find("difficult").text)
+                    # if difficult == 1:
+                    #     continue
+                    if cls != cls_:
+                        continue
+                    bbox = obj.find("bndbox")
+                    bbox = [float(bbox.find(x).text) for x in ["xmin", "ymin", "xmax", "ymax"]]
+                    bbox[0] -= 1.0
+                    bbox[1] -= 1.0
 
-        r = {
-            "file_name": jpeg_file,
-            "image_id": fileid,
-            "height": int(tree.findall("./size/height")[0].text),
-            "width": int(tree.findall("./size/width")[0].text),
-        }
-        instances = []
+                    instances.append({
+                        "category_id": classnames.index(cls_),
+                        "bbox": bbox,
+                        "bbox_mode": BoxMode.XYXY_ABS
+                    })
+                if tot_instance + len(instances) <= int(shot):
+                    r["annotations"] = instances
+                    tot_instance += len(instances)
+                else:
+                    r["annotations"] = instances[:int(shot)-tot_instance]
+                    tot_instance = int(shot)
+                dicts_.append(r)
+                if tot_instance >= int(shot):
+                    break
+            # if len(dicts_) > int(shot):
+            #     dicts_ = np.random.choice(dicts_, int(shot), replace=False)
+            dicts.extend(dicts_)
+    else:
+        for fileid in fileids:
+            anno_file = os.path.join(dirname, "Annotations", fileid + ".xml")
+            jpeg_file = os.path.join(dirname, "JPEGImages", fileid + ".jpg")
 
-        for obj in tree.findall("object"):
-            cls = obj.find("name").text
-            if not (cls in classnames):
-                continue
-            bbox = obj.find("bndbox")
-            bbox = [float(bbox.find(x).text) for x in ["xmin", "ymin", "xmax", "ymax"]]
-            bbox[0] -= 1.0
-            bbox[1] -= 1.0
+            tree = ET.parse(anno_file)
 
-            instances.append({
-                "category_id": classnames.index(cls),
-                "bbox": bbox,
-                "bbox_mode": BoxMode.XYXY_ABS,
-            })
-        r["annotations"] = instances
-        dicts.append(r)
+            r = {
+                "file_name": jpeg_file,
+                "image_id": fileid,
+                "height": int(tree.findall("./size/height")[0].text),
+                "width": int(tree.findall("./size/width")[0].text),
+            }
+            instances = []
+
+            for obj in tree.findall("object"):
+                cls = obj.find("name").text
+                if not (cls in classnames):
+                    continue
+                difficult = int(obj.find("difficult").text)
+                if difficult == 1:
+                    continue # voc do not use difficult objects to calculate mAP
+                bbox = obj.find("bndbox")
+                bbox = [float(bbox.find(x).text) for x in ["xmin", "ymin", "xmax", "ymax"]]
+                bbox[0] -= 1.0
+                bbox[1] -= 1.0
+
+                instances.append({
+                    "category_id": classnames.index(cls),
+                    "bbox": bbox,
+                    "bbox_mode": BoxMode.XYXY_ABS,
+                })
+            r["annotations"] = instances
+            dicts.append(r)
     return dicts
 
 
